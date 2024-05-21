ebfb6295264cf600e0c9879faa1580c57f5c07d3
Peter Price <peteraprice@btinternet.com>
2024-05-21 15:02:54 +0100

    Must acknowledge any TX FIFO underflow that may have occurred

--- components/cc1101/cc1101.c
+++ components/cc1101/cc1101.c
@@ -137,22 +137,17 @@ void cc_enter_idle_mode(void) {
 }
 
 void cc_enter_rx_mode(void) {
-//  while ( CC_STATE( cc_strobe( CC_SIDLE ) ) != CC_STATE_IDLE ){}
-
   cc_write( CC_IOCFG0, 0x09 );      // Show Clear Channel Assessment
   cc_write( CC_PKTCTRL0, 0x32 );	// Asynchronous, infinite packet
 
-//  cc_strobe( CC_SFRX );
+  cc_strobe( CC_SFTX );
   while ( CC_STATE( cc_strobe( CC_SRX ) ) != CC_STATE_RX ){}
 }
 
 void cc_enter_tx_mode(void) {
-//  while ( CC_STATE( cc_strobe( CC_SIDLE ) ) != CC_STATE_IDLE ){}
-
   cc_write( CC_PKTCTRL0, 0x02 );    // Fifo mode, infinite packet
   cc_write( CC_IOCFG0, 0x02 );      // Falling edge, TX Fifo low
 
-//  cc_strobe( CC_SFTX );
   while ( CC_STATE( cc_strobe( CC_STX ) ) != CC_STATE_TX ){}
 }
 
